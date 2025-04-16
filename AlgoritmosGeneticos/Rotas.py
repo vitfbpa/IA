f94c3d566b6beed60d5063b75c60e3a843160b4b
Vicenzo de Souza <141375589+vitfbpa@users.noreply.github.com>
2025-04-16 11:41:28 -0300

    Add files via upload

---
+++ AlgoritmosGeneticos/Rotas.py
@@ -0,0 +1,20 @@
+import copy
+import random
+
+populacao = list()
+
+rota = list()
+tamanho_populacao = 100
+
+while (tamanho_populacao > 0):
+    for i in range(9):
+        rota.append(i)
+        
+    random.shuffle(rota)
+    populacao.append(copy.deepcopy(rota))
+    tamanho_populacao -= 1
+    rota.clear()
+
+print('Gerando rotas aleatórias...')
+for individuo in populacao:
+    print(individuo)
